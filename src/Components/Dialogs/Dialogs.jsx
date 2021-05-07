605fc74671365d546e09035b30c4533c1cb021cd
Andrey <dfg>
2021-05-07 15:48:16 +0700

    Dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import s from './Dialogs.module.css'
 const Dialogs = (props) =>{
     return (
-        <div >
-            Dialods
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Andrey</div>
+                <div className={s.dialog}>Egor</div>
+                <div className={s.dialog}>Petr</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Elena</div>
+                <div className={s.dialog}>Victor</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>asd</div>
+                <div className={s.message}>tyr</div>
+            </div>
         </div>
     )
 }
