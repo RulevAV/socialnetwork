d13034ecc59b5bbc67820de27126c3aebfcfecdf
Andrey <dfg>
2021-05-07 11:14:17 +0700

    вёрстка, grid

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+        <header className='header'>
+          <img src='https://papik.pro/uploads/posts/2021-01/1610456240_pin_by_sandi_rentz_on_art_animal_drawings_animal_sketches.jpg'/>
+        </header>
+      <nav className='nav'>
+          <div>
+              <a>Profile</a>
+          </div>
+          <div>
+              <a>Messages</a>
+          </div>
+          <div>
+              <a>News</a>
+          </div>
+          <div>
+              <a>Settings</a>
+          </div>
+
+      </nav>
+      <div className='content'>
+          <div>
+          <img src='https://wallpapershome.ru/images/wallpapers/4k-7680x4320-5k-8k-abstrakciya-oboi-249.jpg'/>
+          </div>
+          <div>
+              ava + description
+          </div>
+          <div>
+              My post
+              <div>
+                  new post
+              </div>
+              <div>
+                  post 1
+              </div>
+              <div>
+                  post 2
+              </div>
+          </div>
+
+      </div>
     </div>
   );
 }
