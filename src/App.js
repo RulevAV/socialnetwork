e57d51dfa0c35b6fbd933507a949f3a0076b87c3
Andrey <dfg>
2021-05-07 15:07:46 +0700

    Route и BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
-
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
+      <BrowserRouter>
     <div className="app-wrapper">
         <Header/>
         <Navbar/>
         <div className={'app-wrapper-content'}>
-            {/* <Profile/>*/}
-            <Dialogs/>
+            <Route component={Dialogs} path="/dialogs"/>
+            <Route component={Profile} path="/profile"/>
         </div>
-
     </div>
+      </BrowserRouter>
   );
 }
 
