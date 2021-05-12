e223cb7bf93a2b8c6a5a33e9d29655a5aedb73e8
Andrey <dfg>
2021-05-12 22:20:57 +0700

    local state forceUpdate setState

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import s from "../ProfileInfo/ProfileInfo.module.css";
 import Preloader from "../../Common/Preloader/Preloader";
+import ProfileStatus from "../ProfileStatus";
+
+
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
         return <Preloader/>
     }
     return <div>
-        <div>
-            <img className={s.img}
-                 src='https://wallpapershome.ru/images/wallpapers/4k-7680x4320-5k-8k-abstrakciya-oboi-249.jpg'/>
-        </div>
+
         <div className={s.descriptionBlock}>
             <img className={s.imgAva} src={ props.profile.photoLarge!==null ? "https://localhost:44304/api/Photo/"+props.profile.photoLarge:null}/>
-            ava + description
+            <ProfileStatus status={"asdasd"}/>
         </div>
     </div>
 }
